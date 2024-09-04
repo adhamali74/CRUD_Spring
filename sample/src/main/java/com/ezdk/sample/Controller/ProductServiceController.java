12491235f24469bd616743f9421071c258f0ae5c
adham ali mohamed <ADHmohamed@ezzsteel.com.eg>
2024-09-04 11:40:12 +0300

    Entity

--- sample/src/main/java/com/ezdk/sample/Controller/ProductServiceController.java
+++ sample/src/main/java/com/ezdk/sample/Controller/ProductServiceController.java
@@ -1,7 +1,7 @@
 package com.ezdk.sample.Controller;
 
 import com.ezdk.sample.Middleware.ProductNotfoundException;
-import com.ezdk.sample.Model.Product;
+import com.ezdk.sample.Entity.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
