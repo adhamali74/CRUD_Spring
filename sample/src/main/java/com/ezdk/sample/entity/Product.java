bf4cd6f74b86d3bc71bf9ea169f90b336cb03ffa
adham ali mohamed <ADHmohamed@ezzsteel.com.eg>
2024-09-09 16:27:08 +0300

    return all fields

--- sample/src/main/java/com/ezdk/sample/entity/Product.java
+++ sample/src/main/java/com/ezdk/sample/entity/Product.java
@@ -13,5 +13,16 @@ public class Product {
     private Long id; // Use Long for ID
 
     @Column(name = "product_name")
-    private String name;
+    private String productName; // Correct field name for product name
+
+    @Column(name = "product_description")
+    private String productDescription; // Field for product description
+
+    @Column(name = "stock_quantity")
+    private Integer stockQuantity; // Field for stock quantity
+
+    @Column(name = "date_added")
+    private String dateAdded; // Field for the date added
+
+    // You can also add more fields if your table has additional columns
 }
